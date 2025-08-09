112669c556a7acee4670da4c92814a6ef23c0c6b
„Arash <jamehbozorgarash@gmail.com>
2025-08-09 17:21:40 +0200

    added .gitignore file including .env file for hiding confidential infos like access keys and so on

--- scripts/test_tuya.py
+++ scripts/test_tuya.py
@@ -1,9 +1,20 @@
 from tuya_connector import TuyaOpenAPI
+from dotenv import load_dotenv # loads variables from .env file
+import os # let us communicate with operating system for editing files, .. and raeding variables from .env file
 
-ACCESS_ID = "p73wjpk7xv4k938qeyht"
-ACCESS_KEY = "36a41b4ca24f40909557884006953ccc"
-API_ENDPOINT = "https://openapi.tuyaeu.com"  
-DEVICE_ID = "bfaf0fd8be6effd2d3uhgx"
+load_dotenv() # looks for a file named .env in the current directory, which is scripts folders
+
+# Function for checking if the variables in .env exist or not
+def get_env_var(name: str) -> str:
+    value = os.getenv(name)
+    if value is None:
+        raise ValueError(f"Missing required environment variable: {name}")
+    return value
+
+API_ENDPOINT = get_env_var("API_ENDPOINT")
+ACCESS_ID = get_env_var("ACCESS_ID")
+ACCESS_KEY = get_env_var("ACCESS_KEY")
+DEVICE_ID = get_env_var("DEVICE_ID")
 
 openapi = TuyaOpenAPI(API_ENDPOINT, ACCESS_ID, ACCESS_KEY)
 openapi.connect()
